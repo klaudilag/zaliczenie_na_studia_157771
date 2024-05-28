72294410742c5f84da998c2dc7a5f758f68d3b02
klaudilag <klaudilag@wp.pl>
2024-05-28 19:50:57 +0200

    new files and components

---
+++ src/Components/Header.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material"
+
+export const Header = () => {
+    return (
+        <Box className="header">
+            <Box className="header_right">
+                <h1>Recipe Finder</h1>
+                <h2>Find your favorite recipe!</h2>
+            </Box>
+        </Box>
+    )
+}
